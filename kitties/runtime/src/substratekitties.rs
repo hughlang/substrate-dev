fe15bfa28a3b30bac086095ebfdf3dea837e87f1
Hugh Lang <hughlang@gmail.com>
2019-06-19 10:12:53 -0400

    Refactor + Setting a Price

--- kitties/runtime/src/substratekitties.rs
+++ kitties/runtime/src/substratekitties.rs
@@ -21,10 +21,12 @@ decl_event!(
     pub enum Event<T>
     where
         <T as system::Trait>::AccountId,
-        <T as system::Trait>::Hash
+        <T as system::Trait>::Hash,
+        <T as balances::Trait>::Balance
     {
         // ACTION: Add a `Created` event which includes an `AccountId` and a `Hash`
         Created(AccountId, Hash),
+        PriceSet(AccountId, Hash, Balance),
     }
 );
 
@@ -67,22 +69,9 @@ decl_module! {
         fn create_kitty(origin) -> Result {
             let sender = ensure_signed(origin)?;
 
-            // ACTION: Generate variables `owned_kitty_count` and `new_owned_kitty_count`
-            //         similar to `all_kitties_count` below
-            let owned_kitty_count = Self::num_owned_kitties(&sender);
-            let new_owned_kitty_count = owned_kitty_count.checked_add(1).ok_or("Overflow adding a new kitty")?;
-
-            // ACTION: Get the current `AllKittiesCount` value and store it in `all_kitties_count`
-            // ACTION: Create a `new_all_kitties_count` by doing a `checked_add()` to increment `all_kitties_count`
-            //      REMINDER: Return an `Err()` if there is an overflow
-            let all_kitties_count = Self::num_of_kitties();
-            let new_all_kitties_count = all_kitties_count.checked_add(1).ok_or("Overflow adding a new kitty")?;
-
             let nonce = <Nonce<T>>::get();
             let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
                 .using_encoded(<T as system::Trait>::Hashing::hash);
-
-            ensure!(!<KittyOwner<T>>::exists(random_hash), "Kitty already exists");
 
             let new_kitty = Kitty {
                 id: random_hash,
@@ -90,27 +79,79 @@ decl_module! {
                 price: <T::Balance as As<u64>>::sa(0),
                 gen: 0,
             };
-
-            <Kitties<T>>::insert(random_hash, new_kitty);
-            <KittyOwner<T>>::insert(random_hash, &sender);
-
-            // ACTION: Update the storage for the global kitty tracking
-            //         - `AllKittiesArray` should use the `all_kitties_count` (remember `index` is `count - 1`)
-            //         - `AllKittiesCount` should use `new_all_kitties_count`
-            //         - `AllKittiesIndex` should use `all_kitties_count`
-            <AllKittiesArray<T>>::insert(all_kitties_count, random_hash);
-            <AllKittiesCount<T>>::put(new_all_kitties_count);
-            <AllKittiesIndex<T>>::insert(random_hash, all_kitties_count);
-
-            <OwnedKittiesArray<T>>::insert((sender.clone(), owned_kitty_count), random_hash);
-            <OwnedKittiesCount<T>>::insert(&sender, new_owned_kitty_count);
-            <OwnedKittiesIndex<T>>::insert(random_hash, owned_kitty_count);
+            Self::mint(sender, random_hash, new_kitty)?;
 
             <Nonce<T>>::mutate(|n| *n += 1);
-            Self::deposit_event(RawEvent::Created(sender, random_hash));
+
+            Ok(())
+        }
+        fn set_price(origin, kitty_id: T::Hash, new_price: T::Balance) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            // ACTION: Check that the kitty with `kitty_id` exists
+            ensure!(<Kitties<T>>::exists(kitty_id), "This cat does not exist");
+
+            let owner = Self::owner_of(kitty_id).ok_or("No owner for this kitty")?;
+            ensure!(owner == sender, "You do not own this cat");
+
+            let mut kitty = Self::kitty(kitty_id);
+
+            // ACTION: Set the new price for the kitty
+            kitty.price = new_price;
+
+            // ACTION: Update the kitty in storage
+            <Kitties<T>>::insert(kitty_id, kitty);
+
+            // ACTION: Deposit a `PriceSet` event with relevant data
+            //         - owner
+            //         - kitty id
+            //         - the new price
+            Self::deposit_event(RawEvent::PriceSet(sender, kitty_id, new_price));
 
             Ok(())
         }
     }
 }
 
+
+impl<T: Trait> Module<T> {
+    fn mint(to: T::AccountId, kitty_id: T::Hash, new_kitty: Kitty<T::Hash, T::Balance>) -> Result {
+        // ACTION: Refactored code goes here
+        //   NOTE: Some variables have been renamed to generalize the function
+        //         In the end, you should remember add the following call to `create_kitty()`:
+        //         `Self::mint(sender, random_hash, new_kitty)?;`
+        //   HINT: This `mint()` function has checks which could fail AND will write to storage, so place it carefully
+
+        // ACTION: Generate variables `owned_kitty_count` and `new_owned_kitty_count`
+        //         similar to `all_kitties_count` below
+        let owned_kitty_count = Self::num_owned_kitties(&to);
+        let new_owned_kitty_count = owned_kitty_count.checked_add(1).ok_or("Overflow adding a new kitty")?;
+
+        // ACTION: Get the current `AllKittiesCount` value and store it in `all_kitties_count`
+        // ACTION: Create a `new_all_kitties_count` by doing a `checked_add()` to increment `all_kitties_count`
+        //      REMINDER: Return an `Err()` if there is an overflow
+        let all_kitties_count = Self::num_of_kitties();
+        let new_all_kitties_count = all_kitties_count.checked_add(1).ok_or("Overflow adding a new kitty")?;
+
+        ensure!(!<KittyOwner<T>>::exists(kitty_id), "Kitty already exists");
+
+        <Kitties<T>>::insert(kitty_id, new_kitty);
+        <KittyOwner<T>>::insert(kitty_id, &to);
+
+        // ACTION: Update the storage for the global kitty tracking
+        //         - `AllKittiesArray` should use the `all_kitties_count` (remember `index` is `count - 1`)
+        //         - `AllKittiesCount` should use `new_all_kitties_count`
+        //         - `AllKittiesIndex` should use `all_kitties_count`
+        <AllKittiesArray<T>>::insert(all_kitties_count, kitty_id);
+        <AllKittiesCount<T>>::put(new_all_kitties_count);
+        <AllKittiesIndex<T>>::insert(kitty_id, all_kitties_count);
+
+        <OwnedKittiesArray<T>>::insert((to.clone(), owned_kitty_count), kitty_id);
+        <OwnedKittiesCount<T>>::insert(&to, new_owned_kitty_count);
+        <OwnedKittiesIndex<T>>::insert(kitty_id, owned_kitty_count);
+
+        Self::deposit_event(RawEvent::Created(to, kitty_id));
+
+        Ok(())
+    }
+}
