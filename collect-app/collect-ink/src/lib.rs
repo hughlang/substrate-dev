7756d3237c01da9083639e0ca6129c56264a3c2d
Hugh Lang <hughlang@gmail.com>
2019-07-15 12:21:42 -0400

    Transfer event

--- collect-app/collect-ink/src/lib.rs
+++ collect-app/collect-ink/src/lib.rs
@@ -10,6 +10,12 @@ use ink_lang::contract;
 contract! {
     #![env = DefaultSrmlTypes]
 
+    event Transfer {
+        from: Option<AccountId>,
+        to: Option<AccountId>,
+        value: Balance,
+    }
+
     struct Erc20 {
         /// The total supply.
         total_supply: storage::Value<Balance>,
@@ -22,7 +28,16 @@ contract! {
             // ACTION: `set` the total supply to `init_value`
             // ACTION: `insert` the `init_value` as the `env.caller()` balance
             self.total_supply.set(init_value);
-            self.balances.insert(env.caller(), init_value);        }
+            self.balances.insert(env.caller(), init_value);
+            // ACTION: Call `env.emit` with the `Transfer` event
+            //   HINT: According to the ERC20 specification, we should set from to `None`
+            //   HINT: Since we use `Option<AccountId>`, you need to wrap accounts in `Some()`
+            env.emit(Transfer {
+                from: None,
+                to: Some(env.caller()),
+                value: init_value
+            });
+        }
     }
 
     impl Erc20 {
@@ -44,6 +59,12 @@ contract! {
             env.println(&format!("Erc20::balance_of(owner = {:?}) = {:?}", owner, balance));
             balance
         }
+
+        /// Transfers token from the sender to the `to` AccountId.
+        pub(external) fn transfer(&mut self, to: AccountId, value: Balance) -> bool {
+            // ACTION: Call the `transfer_impl` with `from` as `env.caller()`
+            self.transfer_impl(env, env.caller(), to, value)
+        }
     }
 
     impl Erc20 {
@@ -53,8 +74,34 @@ contract! {
             // ACTION: Return the balance
             *self.balances.get(of).unwrap_or(&0)
         }
+
+        /// Transfers token from a specified AccountId to another AccountId.
+        fn transfer_impl(&mut self, env: &mut ink_model::EnvHandler<ink_core::env::ContractEnv<DefaultSrmlTypes>>, from: AccountId, to: AccountId, value: Balance) -> bool {
+            // ACTION: Get the balance for `from` and `to`
+            //   HINT: Use the `balance_of_or_zero` function to do this
+            // ACTION: If `balance` from is less than `value`, return `false`
+            // ACTION: Insert new values for `from` and `to`
+            //         * balance_from - value
+            //         * balance_to + value
+            let balance_from = self.balance_of_or_zero(&from);
+            let balance_to = self.balance_of_or_zero(&to);
+            if balance_from < value {
+                return false
+            }
+            self.balances.insert(from, balance_from - value);
+            self.balances.insert(to, balance_to + value);
+            // ACTION: Call `env.emit` with the `Transfer` event
+            //   HINT: Since we use `Option<AccountId>`, you need to wrap accounts in `Some()`
+            env.emit( Transfer {
+                from: Some(from),
+                to: Some(to),
+                value
+            });
+            true
+        }
     }
 }
+
 
 #[cfg(all(test, feature = "test-env"))]
 mod tests {
@@ -73,5 +120,38 @@ mod tests {
         assert_eq!(erc20.total_supply(), 1234);
         // Check that `balance_of` Alice is `init_value`
         assert_eq!(erc20.balance_of(alice), 1234);
+    }
+
+    #[test]
+    fn transfer_works() {
+        let alice = AccountId::from([0x0; 32]);
+        let bob = AccountId::from([0x1; 32]);
+
+        env::test::set_caller::<Types>(alice);
+        // Deploy the contract with some `init_value`
+        let mut erc20 = Erc20::deploy_mock(1234);
+        // Alice does not have enough funds for this
+        assert_eq!(erc20.transfer(bob, 4321), false);
+        // Alice can do this though
+        assert_eq!(erc20.transfer(bob, 234), true);
+        // Check Alice and Bob have the expected balance
+        assert_eq!(erc20.balance_of(alice), 1000);
+        assert_eq!(erc20.balance_of(bob), 234);
+    }
+
+    #[test]
+    fn events_work() {
+        let alice = AccountId::from([0x0; 32]);
+        let bob = AccountId::from([0x1; 32]);
+
+        // No events to start
+        env::test::set_caller::<Types>(alice);
+        assert_eq!(env::test::emitted_events::<Types>().count(), 0);
+        // Event should be emitted for initial minting
+        let mut erc20 = Erc20::deploy_mock(1234);
+        assert_eq!(env::test::emitted_events::<Types>().count(), 1);
+        // Event should be emitted for transfers
+        assert_eq!(erc20.transfer(bob, 10), true);
+        assert_eq!(env::test::emitted_events::<Types>().count(), 2);
     }
 }
